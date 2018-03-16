d0d769c221887acc5117a966f780f6cc4512a40e
dhopki12 <davehops@jhu.edu>
2018-03-15 20:59:24 -0400

    remove .pyc files and test order of operations in source file

--- nixSetup.sh
+++ nixSetup.sh
@@ -1,6 +1,6 @@
+virtualenv venv
 source venv/bin/activate
 export FLASK_APP="run.py"
 export SECRET="c9cef5b979b3fcae3bc82bc4c44e95ac"
 export APP_SETTINGS="development"
 export DATABASE_URL="sqlite:///bucketlist.db"
-virtualenv venv
