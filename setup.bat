77e5ccba470bcbad3168aa7cc69f4f2812aa0f5d
davehops <davehops@gmail.com>
2018-03-20 19:20:05 -0400

    update setup files for windows

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
+virtualenv venv
 call venv\Scripts\activate
 set FLASK_APP=run.py
 set SECRET=c9cef5b979b3fcae3bc82bc4c44e95ac
 set APP_SETTINGS=development
 set DATABASE_URL=sqlite:///bucketlist.db
-virtualenv venv
